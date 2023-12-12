67bad6b28f64c7ac2256bd047d5069afcb85ed44
Đỗ Xuân Hiểu <91748021+doxuanhieu185@users.noreply.github.com>
2023-12-12 17:44:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-
-
 from keras.models import load_model
-from time import sleep
 from keras.preprocessing.image import img_to_array
-from keras.preprocessing import image
 import cv2
 import numpy as np
 
 face_classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 classifier =load_model('./Emotion_Detection.h5')
-
-class_labels = ['Angry','Happy','Neutral','Sad','Surprise']
+videopath = './abnormal.mp4'
+class_labels = ['tuc gian','vui ve','binh thuong','buon','bat ngo']
 
 cap = cv2.VideoCapture(1)
 status = []
 
 
 while True:
-    # Grab a single frame of video
+    # Lấy một khung hình video
     ret, frame = cap.read()
     if ret is False or frame is None:  # Kiểm tra xem frame có giá trị hay không
         break
     labels = []
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
+
 
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
@@ -38,11 +35,8 @@ while True:
 
 
             preds = classifier.predict(roi)[0]
-            print("\nprediction = ",preds)
             label=class_labels[preds.argmax()]
-            print("\nprediction max = ",preds.argmax())
             status.append(preds.argmax())
-            print("\nlabel = ",label)
             label_position = (x,y)
             cv2.putText(frame,label,label_position,cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
         else:
@@ -51,14 +45,14 @@ while True:
     cv2.imshow('Emotion Detector',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-cap.release()
-cv2.destroyAllWindows()
+
 cnt_status = len(set(status))
 if cnt_status >=3:
     print("có dấu hiệu tự kỷ !!")
 else:
     print("Bình thường")
-
+cap.release()
+cv2.destroyAllWindows()
 
 
 
