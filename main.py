4dfede930082d3be96124b0ef55a4e457d71576c
Đỗ Xuân Hiểu <91748021+doxuanhieu185@users.noreply.github.com>
2023-12-12 17:46:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ while True:
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
 
-
+# ve hinh chu nhat xung quanh khuon mat
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h,x:x+w]
         roi_gray = cv2.resize(roi_gray,(48,48),interpolation=cv2.INTER_AREA)
 
-
+# neu vung trong tam co gia tri thi convert sang mang
         if np.sum([roi_gray])!=0:
             roi = roi_gray.astype('float')/255.0
             roi = img_to_array(roi)
             roi = np.expand_dims(roi,axis=0)
 
-
+# nhan dien cam xuc
             preds = classifier.predict(roi)[0]
             label=class_labels[preds.argmax()]
             status.append(preds.argmax())
